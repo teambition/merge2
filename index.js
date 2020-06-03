5dc3ea60a18feed7fd26f29ec2f06ddb89bf88cd
zensh <admin@zensh.com>
2020-06-03 13:53:23 +0800

    release v1.4.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * merge2
  * https://github.com/teambition/merge2
  *
- * Copyright (c) 2014-2016 Teambition
+ * Copyright (c) 2014-2020 Teambition
  * Licensed under the MIT license.
  */
 const Stream = require('stream')
