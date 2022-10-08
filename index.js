bc86a3dbacc035e229b68ca8bbf705d770fd395a
zensh <admin@zensh.com>
2022-10-08 13:52:07 +0800

    update License date.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * merge2
  * https://github.com/teambition/merge2
  *
- * Copyright (c) 2014-2020 Teambition
+ * Copyright (c) 2014-2022 Teambition
  * Licensed under the MIT license.
  */
 const Stream = require('stream')
